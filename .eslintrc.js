24dfe4f6d606a374d03f15486e50d1f95ac1dc16
Developmenteng <maqan3@hotmail.com>
2026-06-28 20:06:22 +0300

    rearange the local Repo structure

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['expo'],
-  env: {
-    jest: true,
-  },
-  ignorePatterns: ['node_modules/', 'dist/', 'android/', 'ios/'],
-};
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'jest-expo',
-  testMatch: ['**/__tests__/**/*.test.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-?expo|@react-native|react-native|expo(nent)?|@expo(nent)?/.*|expo-modules-core|react-navigation|@react-navigation/.*)/)',
-  ],
-};
